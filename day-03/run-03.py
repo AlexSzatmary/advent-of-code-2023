0cb7a510d37147fdabc527761306c43f66e73f63
Alex C. Szatmary <alex.szatmary@gmail.com>
2023-12-03 20:24:06 -0500

    Solve 3_1

---
+++ day-03/run-03.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import numpy as np
+import re
+import sys
+
+
+def syms_from_L(L):
+    return np.array(
+        [[1 if (not c.isnumeric() and c != ".") else 0 for c in s[:-1]] for s in L]
+    )
+
+
+def add_matching_parts(L):
+    syms = syms_from_L(L)
+    count = 0
+    for i, s in enumerate(L):
+        for m in re.finditer(r"\d+", s):
+            # breakpoint()
+            if np.any(syms[max(0, i - 1):min(i + 1 + 1, np.size(syms, 0)),
+                           max(0, m.start() - 1):min(m.end() + 1, np.size(syms, 1))]):
+                count += int(m.group())
+                # print(int(m.group()))
+    return count
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    with open(argv[1], "r") as hin:
+        L = hin.readlines()
+        print(add_matching_parts(L))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
