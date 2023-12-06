ab9b11331d9ce7ee38443837396b44ca1d479ca5
Alex C. Szatmary <alex.szatmary@gmail.com>
2023-12-05 21:39:30 -0500

    Set up and solve 05_1

---
+++ day-05/run_05_01.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# import numpy as np
+import re
+import sys
+
+
+def parse_tables(L):
+    seeds = list(map(int, re.findall(r"\d+", L[0])))
+    maps = []
+    map_names = []
+    for s in L[1:]:
+        if m := re.match(r"(.*):", s):
+            map_names.append(m.group(1))
+            maps.append([])
+        elif m := re.match(r"(\d+) (\d+) (\d+)", s):
+            maps[-1].append(tuple(map(int, m.groups())))
+        else:
+            pass
+    return seeds, map_names, maps
+
+
+def apply_map(i, m):
+    for dest, src, range in m:
+        if src <= i < src + range:
+            return i - src + dest
+    return i
+
+
+def apply_all_maps(seed, maps):
+    i = seed
+    for m in maps:
+        i = apply_map(i, m)
+    return i
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    with open(argv[1], "r") as hin:
+        L = hin.readlines()
+        seeds, map_names, maps = parse_tables(L)
+        locs = [apply_all_maps(seed, maps) for seed in seeds]
+        print(min(locs))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
