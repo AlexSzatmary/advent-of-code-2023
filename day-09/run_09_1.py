bd1c34cf838ca0173049b2725217b5f3c61da88a
Alex C. Szatmary <alex.szatmary@gmail.com>
2023-12-10 22:21:22 -0500

    Solve 09_1 and 09_2

---
+++ day-09/run_09_1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# from itertools import cycle
+import re
+import sys
+
+
+def parse_input(L):
+    rows = []
+    for s in L:
+        rows.append(list(map(int, re.findall(r"(-?\d+)", s))))
+    return rows
+
+
+def predict(row):
+    print(row)
+    tableau = []
+    tableau.append([row[i + 1] - row[i] for i in range(len(row) - 1)])
+    while not all(x == 0 for x in tableau[-1]):
+        r = tableau[-1]
+        tableau.append([r[i + 1] - r[i] for i in range(len(r) - 1)])
+    lower_end = 0
+    for r in reversed(tableau):
+        lower_end += r[-1]
+    return row[-1] + lower_end
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    with open(argv[1], "r") as hin:
+        L = hin.readlines()
+    rows = parse_input(L)
+    print(len(rows))
+    print(sum(map(predict, rows)))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
