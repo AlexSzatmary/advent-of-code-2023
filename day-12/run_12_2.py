b1e86e7ad327d1d211101ffec6a9f191e613a74a
Alex C. Szatmary <alex.szatmary@gmail.com>
2023-12-16 13:02:17 -0500

    Solves 12_2 efficiently
    Use good re, good memo function, functools.cache

--- day-12/run_12_2.py
+++ day-12/run_12_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # from itertools import cycle
+from functools import cache
+import timeit
 import re
 import sys
 
@@ -9,7 +11,7 @@ def parse_input(L):
     for s in L:
         row, groups = s.split(" ")
         repeats = 5
-        row = "?".join(row for i in range(repeats))
+        row = "?".join(row for i in range(repeats)) + "."
         # print(groups[:-1])
         groups = ",".join(groups[:-1] for i in range(repeats))
         # print(row, groups)
@@ -17,24 +19,25 @@ def parse_input(L):
     return rows
 
 
-def pattern_from_groups(groups):
-    return (
-        r"^([.?]*?)"
-        + r"[.?]+".join(r"[#?]{" + str(i) + "}" for i in groups)
-        + r"[^#]*$"
-    )
+def pattern_from_groups(group):
+    return r"^([.?]*?)" + r"[#?]{" + str(group) + "}" + r"[^#]"
 
 
-def count_arrangements(row, groups):
-    # breakpoint()
-    if not groups:
-        return 1
-    m = re.match(pattern_from_groups(groups), row)
+@cache
+def count_arrangements(row, groups, start_row, start_groups):
+    if start_groups >= len(groups):
+        if "#" in row[start_row:]:
+            return 0
+        else:
+            return 1
+    m = re.match(pattern_from_groups(groups[start_groups]), row[start_row:])
     if m:
-        start = len(m.group(1))
-        dig = count_arrangements(row[start + groups[0] + 1 :], groups[1:])
-        if row[start].startswith("?"):
-            scan = count_arrangements(row[len(m.group(1)) + 1 :], groups)
+        start_row += len(m.group(1))
+        dig = count_arrangements(
+            row, groups, start_row + groups[start_groups] + 1, start_groups + 1
+        )
+        if row[start_row].startswith("?"):
+            scan = count_arrangements(row, groups, start_row + 1, start_groups)
         else:
             scan = 0
         return dig + scan
@@ -47,13 +50,20 @@ def main(argv=None):
         argv = sys.argv
     with open(argv[1], "r") as hin:
         L = hin.readlines()
+    start = timeit.default_timer()
     rows = parse_input(L)
     possibilities = 0
-    for row, groups in rows:
-        p = count_arrangements(row, groups) 
-        print(row, groups, p)
+    for i, (row, groups) in enumerate(rows):
+        for j in range(4, 0, -1):  # precompute segments from later
+            count_arrangements(
+                row, tuple(groups), len(row) * j // 5, len(groups) * j // 5
+            )
+        p = count_arrangements(row, tuple(groups), 0, 0)
+        print("***", i, "***", row, groups, p)
         possibilities += p
     print(possibilities)
+    stop = timeit.default_timer()
+    print("Time:", stop - start)
 
 
 if __name__ == "__main__":
