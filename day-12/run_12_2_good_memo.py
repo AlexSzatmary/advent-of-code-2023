9575475d5637f425a3bbdab9dba3f3b6db361275
Alex C. Szatmary <alex.szatmary@gmail.com>
2023-12-15 22:42:01 -0500

    This is like fine but without my_row and my_groups

---
+++ day-12/run_12_2_good_memo.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# from itertools import cycle
+import timeit
+import re
+import sys
+
+
+def parse_input(L):
+    rows = []
+    for s in L:
+        row, groups = s.split(" ")
+        repeats = 5
+        row = "?".join(row for i in range(repeats))
+        # print(groups[:-1])
+        groups = ",".join(groups[:-1] for i in range(repeats))
+        # print(row, groups)
+        rows.append((row, list(map(int, groups.split(",")))))
+    return rows
+
+
+def pattern_from_groups(groups):
+    return (
+        r"^([.?]*?)"
+        + r"[.?]+".join(r"[#?]{" + str(i) + "}" for i in groups)
+        + r"[^#]*$"
+    )
+
+
+def count_arrangements(row, groups, start_row, start_groups, memo):
+    if (start_row, start_groups) in memo:
+        return memo[(start_row, start_groups)]
+    if start_groups >= len(groups):
+        return 1
+    m = re.match(pattern_from_groups(groups[start_groups:]), row[start_row:])
+    if m:
+        start_row += len(m.group(1))
+        dig = count_arrangements(
+            row, groups, start_row + groups[start_groups] + 1, start_groups + 1, memo
+        )
+        if row[start_row].startswith("?"):
+            scan = count_arrangements(row, groups, start_row + 1, start_groups, memo)
+        else:
+            scan = 0
+        return dig + scan
+    else:
+        return 0
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    with open(argv[1], "r") as hin:
+        L = hin.readlines()
+    start = timeit.default_timer()
+    rows = parse_input(L)
+    possibilities = 0
+    for i, (row, groups) in enumerate(rows):
+        p = count_arrangements(row, groups, 0, 0, {})
+        print("***", i, "***", row, groups, p)
+        possibilities += p
+    print(possibilities)
+    stop = timeit.default_timer()
+    print("Time:", stop - start)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
