3a13f116be0c44ad57a76e581ccc77762a7f4146
Alex C. Szatmary <alex.szatmary@gmail.com>
2024-01-01 15:19:10 -0500

    Solve 25_1

---
+++ day-25/run_25_1.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+from collections import Counter
+import re
+import sys
+import timeit
+
+
+def parse_input(L):
+    graph = {}
+    for s in L:
+        (node, *edges) = re.findall(r"\w+", s)
+        graph[node] = edges
+    return graph
+
+
+def make_undirected(graph):
+    edges_to_add = []
+    for node in graph:
+        for edge in graph[node]:
+            if edge not in graph:
+                edges_to_add.append(edge)
+    for node in edges_to_add:
+        graph[node] = []
+    for node in graph:
+        for edge in graph[node]:
+            if node not in graph[edge]:
+                graph[edge].append(node)
+    return graph
+
+
+def make_numeric(graph):
+    indices = {node: i for i, node in enumerate(graph)}
+    ngraph = {}
+    for node in graph:
+        ngraph[indices[node]] = Counter(indices[edge] for edge in graph[node])
+    return ngraph
+
+
+def merge_highly_connected(graph, cabals):
+    most_connected = 0
+    best_pals = None
+    for a in graph:
+        for b in graph[a]:
+            k = (
+                sum(min(graph[a][c], graph[b][c]) for c in graph[a] if c in graph[b])
+                + graph[a][b]
+            )
+            if k > most_connected:
+                best_pals = (a, b)
+                most_connected = k
+
+    (a, b) = best_pals
+    for c in graph[b]:
+        if c != a:
+            graph[c][a] += graph[c].pop(b)
+    graph[a] += graph[b]
+    graph[a].pop(a)
+    graph[a].pop(b)
+    graph.pop(b)
+
+    for i in range(len(cabals)):
+        if b in cabals[i]:
+            cabal_b = cabals.pop(i)
+            break
+    for cabal in cabals:
+        if a in cabal:
+            cabal.update(cabal_b)
+            break
+
+    return graph, cabals
+
+
+def run_merge_highly_connected(graph):
+    cabals = [set([node]) for node in graph]
+    while len(cabals) > 2:
+        graph, cabals = merge_highly_connected(graph, cabals)
+        for node in graph:
+            if graph[node].total() == 3:
+                for cabal in cabals:
+                    if node in cabal:
+                        return len(cabal) * (sum(len(c) for c in cabals) - len(cabal))
+                break
+    print("This should not execute")
+    return graph, cabals
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    with open(argv[1], "r") as hin:
+        L = hin.readlines()
+    start = timeit.default_timer()
+    graph = make_numeric(make_undirected(parse_input(L)))
+
+    if len(argv) == 3 and argv[2] == "dot":
+        print("graph {")
+        for node, edges in graph.items():
+            print(f"{node} -- {{{' '.join(map(str, edges))}}}")
+        print("}")
+        sys.exit()
+
+    print(run_merge_highly_connected(graph))
+    stop = timeit.default_timer()
+    print("Time:", stop - start)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
